e0e2c5d8892bcfb608e115d0ebd39c96ea3ee6f5
BgirlStar <you@example.com>
2017-04-01 17:07:23 +0800

    final

--- js/need.js
+++ js/need.js
@@ -1,12 +1,14 @@
  function check(){
  				if (document.myForm.email.value == "" || document.myForm.password.value == "") {
- 					alert("Please input your email and password!");
+ 					//alert("Please input your email and password!");
+					Showbo.Msg.alert("Please input your email and password!");
  					document.myForm.reset();
  				} else if((document.myForm.email.value=="cxr@qq.com"&& document.myForm.password.value =="123") || (document.myForm.email.value=="lm@qq.com"&& document.myForm.password.value=="321")){
- 					alert("Success!");
+ 					//alert("Success!");
+					Showbo.Msg.alert("Success");
  					document.myForm.reset();
  				}else{
-                alert("The user name or password you entered is incorrect!"); 
+                Showbo.Msg.alert("The user name or password you entered is incorrect!"); 
                  document.myForm.reset();              
              } 
   }
